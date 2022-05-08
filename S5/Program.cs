4b73e3b6804dfb2b965a1e77d7ba37a6115608c1
tarakavian84@gmail.com <tarakavian84@gmail.com>
2022-05-08 19:23:31 +0430

    Add project files.

---
+++ S5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name? ");
+            string name = Console.ReadLine();
+            Console.WriteLine("What is your last name? ");
+            string Lname = Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("You are {0} {1}", name, Lname);
+            Console.ReadKey();
+        }
+    }
+}
